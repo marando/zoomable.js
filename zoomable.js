228f74a6e1a21fc58ee66c5eea9f6d872f61f9a2
Ashley Marando <a.marando@me.com>
2016-02-22 21:24:26 -0500

    add some comments

--- zoomable.js
+++ zoomable.js
@@ -1,113 +1,105 @@
 jQuery.fn.zoomable = function(options) {
 
+  // Do for each element
   $(this).each(function() {
 
+    // Initialize options
+    options = initOptions(options, $(this));
 
-options = initOptions(options, $(this));
+    // Preload images
+    $('<img/>').src = options.fullsize;
 
-  // Preload images
-  $('<img/>').src = options.fullsize;
+    // Initialize the fullsize image
+    var img = $('<img />', {
+      'src': options.fullsize,
+    })    
+    img.css({
+      'max-height': '100%',
+      'max-width': '100%',
+      'position': 'relative',
+      'top': '50%',
+      'transform': 'translateY(-50%)',
+      'border': options.border,
+      'border-radius': options.radius,
+      'box-shadow': options.shadow,
+    });
 
-  var img = $('<img />', {
-    'src': options.fullsize,
-  })
-  img.css({
-    'max-height': '100%',
-    'max-width': '100%',
-    'position': 'relative',
-    'top': '50%',
-    'transform': 'translateY(-50%)',
-    'border': options.border,
-    'border-radius': options.radius,
-    'box-shadow': options.shadow,
-  });
+    // Initialize the container for the fullsize image
+    var container = $('<div />', {
+      'class': 'zoomable-container',
+      'html': img,
+    });
+    container.css({
+      'background-color': options.bgcolor,
+      'padding': options.padding,
+      'text-align': 'center',
+      'position': 'fixed',
+      'top': '0',
+      'left': '0',
+      'z-index': 9999999,
+      'height': '100%',
+      'width': '100%',
+      'display': 'none',
+    });
 
-  var container = $('<div />', {
-    'class': 'zoomable-container',
-    'html': img,
-  });
-  container.css({
-    /*'background-color': $('body').css('background-color')
-                                 .replace('rgb', 'rgba')
-                                 .replace(')', '') + ', 0.8)',*/
-    'background-color': options.bgcolor,
-    'padding': options.padding,
-    'text-align': 'center',
-    'position': 'fixed',
-    'top': '0',
-    'left': '0',
-    'z-index': 100000,
-    'height': '100%',
-    'width': '100%',
-    'display': 'none',
-  });
+    // Apply the opacity to the container
+    container.css({
+      'background-color': container.css('background-color')
+        .replace('rgb', 'rgba')
+        .replace(')', '') + ', ' + options.opacity + ')'
+    });
 
-  container.css({
-    'background-color': container.css('background-color')
-      .replace('rgb', 'rgba')
-      .replace(')', '') + ', ' + options.opacity + ')'
-  });
+    // Container click event (dismiss full size image)
+    container.click(function() {
+      $(this).fadeOut(options.speed);
 
-  container.click(function() {
-    $(this).fadeOut(options.speed);
-   
-    $({blurRadius: options.blur.replace('px', '')}).animate({blurRadius: 0}, {
+      var blurNoPx = options.blur.replace('px', '');
+      $({blurRadius: blurNoPx}).animate({blurRadius: 0}, {
         duration: options.speed,
         easing: 'swing', // or "linear"
-                         // use jQuery UI or Easing plugin for more options
+        // use jQuery UI or Easing plugin for more options
         step: function() {
-             $('.container').css({
-                "-webkit-filter": "blur("+this.blurRadius+"px)",
-                "filter": "blur("+this.blurRadius+"px)"
-            });
+          $('.container').css({
+            "-webkit-filter": "blur(" + this.blurRadius + "px)",
+            "filter": "blur(" + this.blurRadius + "px)"
+          });
         }
+      });
+
+      // Enable body scrolling
+      $('html, body').css({
+        'overflow': 'auto',
+        'height': 'auto'
+      });
     });
 
-    $('html, body').css({
-      'overflow': 'auto',
-      'height': 'auto'
-    });
-  });
-
-
-
-  $(this).click(function() {
-    container.appendTo('body').fadeIn(options.speed);
-    
-    /*var filterVal = 'blur(' + options.blur + ')';
-    $('.container')
-      .css('filter', filterVal)
-      .css('webkitFilter', filterVal)
-      .css('mozFilter', filterVal)
-      .css('oFilter', filterVal)
-      .css('msFilter', filterVal);*/
-
-    $({blurRadius: 0}).animate({blurRadius: options.blur}, {
+    // Thumb click event (shows full size image)
+    $(this).click(function() {
+      container.appendTo('body').fadeIn(options.speed);
+      $({blurRadius: 0}).animate({blurRadius: options.blur}, {
         duration: options.speed,
         easing: 'swing', // or "linear"
-                         // use jQuery UI or Easing plugin for more options
+        // use jQuery UI or Easing plugin for more options
         step: function() {
-             $('.container').css({
-                "-webkit-filter": "blur("+this.blurRadius+"px)",
-                "filter": "blur("+this.blurRadius+"px)"
-            });
+          $('.container').css({
+            "-webkit-filter": "blur(" + this.blurRadius + "px)",
+            "filter": "blur(" + this.blurRadius + "px)"
+          });
         }
+      });
+
+      // Disable body scrolling
+      $('html, body').css({
+        'overflow': 'hidden',
+        'height': '100%'
+      });
     });
-
-
-
-    $('html, body').css({
-      'overflow': 'hidden',
-      'height': '100%'
-    });
-  });
-
 
   });
 
 };
 
-function initOptions(options, parent) {  
+function initOptions(options, parent) {
   // Use default options if none were passed on call
   options = $.extend({}, $.fn.zoomable.defaults, options);
 
